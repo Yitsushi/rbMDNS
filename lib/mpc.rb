dd4e7b252d7b6ea4bbb2a3d9bafe9d4c0afec9ea
Raito Yitsushi <yitsushi@gmail.com>
2010-04-26 00:33:50 +0200

    Add other handlers Fix NoArtist/NoAlbum/NoTitle bug

--- lib/mpc.rb
+++ lib/mpc.rb
@@ -20,6 +20,8 @@ module MDNS
     def initialize
       load_config
 
+      @listeners_ok = false
+      @first_run = true
       @applications = []
 
       load_application_handlers
@@ -43,6 +45,9 @@ module MDNS
     # callback functions
     def state_callback( newstate )
       @state = newstate
+      if newstate == 'play'
+        current_callback(nil)
+      end
       if newstate == 'stop'
         send_to_all(
           @config['message']['state']['stop']['text'],
@@ -53,10 +58,56 @@ module MDNS
 
     def current_callback( song )
       return if @state == 'stop'
+      song = @mpd.current_song if song.nil?
+
       send_to_all(
         generate_satus_message_with_format(song),
         @config['message']['song']['status']
       )
+      puts "D: " + generate_satus_message_with_format(song)
+    end
+
+    def playlist_callback( playlist )
+      puts "Playlist Change #{playlist}"
+    end
+
+    def time_callback( second, full )
+      puts "Time Change #{second}/#{full}"
+    end
+
+    def volume_callback( volume )
+      puts "Volume Change #{volume}"
+    end
+
+    def repeat_callback( repeat )
+      puts "Repeat Change #{repeat}"
+    end
+    
+    def random_callback( random )
+      puts "Random Change #{random}"
+    end
+
+    def playlist_length_callback( pl_length )
+      puts "Playlist Length Change #{pl_length}"
+    end
+    
+    def crossfade_callback( crossfade )
+      puts "Crossfade Change #{crossfade}"
+    end
+
+    def connection_callback( connection )
+      puts "Connection Change #{connection}"
+      puts "#{@first_run} #{connection}"
+      if @first_run and connection
+#       @mpd.register_callback(self.method('time_callback'),  MPD::TIME_CALLBACK)
+        @mpd.register_callback(self.method('volume_callback'),  MPD::VOLUME_CALLBACK)
+      end
+
+      if not connection
+        @mpd.connect(true)
+        # set_listeners
+      end
+      @first_run = false
     end
 
     # Private functions
@@ -71,9 +122,9 @@ module MDNS
     def generate_satus_message_with_format(song)
       format = @config['message']['song']['format']
       format.
-        gsub(/\$\{artist\}/, song.artist).
-        gsub(/\$\{title\}/,  song.title ).
-        gsub(/\$\{album\}/,  song.album )
+        gsub(/\$\{artist\}/, (song.artist.nil? ? "[no artist]" : song.artist) ).
+        gsub(/\$\{title\}/,  (song.title.nil? ? "[no title]" : song.title) ).
+        gsub(/\$\{album\}/,  (song.album.nil? ? "[no album]" : song.album) )
     end
 
     # send 'message' to applications
@@ -85,11 +136,17 @@ module MDNS
     end
 
     def set_listeners
-      current_cb = self.method 'current_callback'
-      @mpd.register_callback(current_cb, MPD::CURRENT_SONG_CALLBACK)
-      
-      state_cb = self.method 'state_callback'
-      @mpd.register_callback(state_cb,  MPD::STATE_CALLBACK)
+      @mpd.register_callback(self.method('current_callback'), MPD::CURRENT_SONG_CALLBACK)
+      @mpd.register_callback(self.method('state_callback'),  MPD::STATE_CALLBACK)
+      @mpd.register_callback(self.method('playlist_callback'),  MPD::PLAYLIST_CALLBACK)
+
+      @mpd.register_callback(self.method('repeat_callback'),  MPD::REPEAT_CALLBACK)
+      @mpd.register_callback(self.method('random_callback'),  MPD::RANDOM_CALLBACK)
+      @mpd.register_callback(self.method('playlist_length_callback'),  MPD::PLAYLIST_LENGTH_CALLBACK)
+      @mpd.register_callback(self.method('crossfade_callback'),  MPD::CROSSFADE_CALLBACK)
+      @mpd.register_callback(self.method('connection_callback'),  MPD::CONNECTION_CALLBACK)
+
+      @listeners_ok = false
     end
   end
 end
